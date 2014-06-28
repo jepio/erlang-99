8c28a7b05c312615b2c1c5108ef08d01b297c891
jepio <jeremi.piotrowski@gmail.com>
2014-06-28 11:27:05 +0200

    Added: all arithmetic problems solved.

--- arith_prb.erl
+++ arith_prb.erl
@@ -1,6 +1,9 @@
 -module(arith_prb).
 -import(list_prb,[range/2,encode/1]).
--compile(export_all).
+-export([is_prime/1,prime_fact/1,enc_prime_fact/1,list_prime/2,goldbach/1,
+         goldbach_list/2,goldbach_list/3,gcd/2,coprime/2,phi/1,phi2/1,
+         test_phi/1,test_avg/1]).
+%%-compile(export_all).
 
 %% Solutions to the Arithmetic problems
 %%
@@ -35,7 +38,14 @@ prime_fact(X,N,Acc) ->
 
 %% 2.03 Encoded prime factors
 enc_prime_fact(X) ->
-    encode(prime_fact(X)).
+    EX=encode(prime_fact(X)),
+    reverse_elements(EX).
+reverse_elements(List) ->
+    reverse_elements(List,[]).
+reverse_elements([],Acc) ->
+    lists:reverse(Acc);
+reverse_elements([H|T],Acc) ->
+    reverse_elements(T,[lists:reverse(H)|Acc]).
 
 %% 2.04 List of prime numbers in range
 list_prime(Low,High) when Low < 3 ->
@@ -69,3 +79,70 @@ goldbach(X,Y) ->
             goldbach(X,Y+2)
     end.
 
+%% 2.06 A list of Goldbach compositions
+goldbach_list(Start,Stop) -> goldbach_list(Start,Stop,0).
+goldbach_list(Start,Stop,Limit) ->
+    case Start rem 2 of
+        1 -> goldbach_list(Start+1,Stop,[],Limit);
+        0 -> goldbach_list(Start,Stop,[],Limit)
+    end.
+goldbach_list(Curr,Stop,_Acc,_) when Curr > Stop ->
+    %lists:reverse(Acc);
+    ok;
+goldbach_list(Curr,Stop,Acc,Limit) ->
+    case goldbach(Curr) of
+        [Lo, Hi] when Lo > Limit ->
+            NewAcc = [[Curr,[Lo,Hi]]|Acc],
+            io:format("~B = ~B + ~B ~n",[Curr|[Lo,Hi]]);
+        _Else ->
+            NewAcc = Acc
+    end,
+    goldbach_list(Curr+2,Stop,NewAcc,Limit).
+
+%% 2.07 Greatest common divisor
+gcd(N,M) when M < N ->
+    gcd(M,N);
+gcd(0,M) -> M;
+gcd(N,M) -> gcd(M rem N,N).
+
+%% 2.08 Coprime
+coprime(X,Y) -> coprime(X,Y,gcd(X,Y)).
+coprime(_X,_Y,1) -> yes;
+coprime(_X,_Y,_) -> no.
+
+%% 2.09 Euler's totient function
+phi(1) -> 1;
+phi(X) ->
+    Nums = range(1,X-1),
+    phi(X,Nums,0).
+phi(_X,[],Acc) -> Acc;
+phi(X,[H|T],Acc) ->
+    case coprime(X,H) of
+        yes -> phi(X,T,Acc+1);
+        no -> phi(X,T,Acc)
+    end.
+
+%% 2.10 Other way of calculating Euler's totient
+phi2(X) ->
+    phi2(X,enc_prime_fact(X),1).
+phi2(X,[[P,M]|T],Acc) ->
+    phi2(X,T,Acc*(P-1)*pow(P,M-1));
+phi2(_,[],Acc) -> Acc.
+
+pow(X,Y) -> pow(X,Y,X).
+pow(_,0,_) -> 1;
+pow(_,1,Acc) -> Acc ;
+pow(X,Y,Acc) -> pow(X,Y-1,Acc*X).
+
+%% 2.11 Testing the performance of both methods
+test_phi(X) ->
+    {T1,V1} = timer:tc(arith_prb,phi,[X]),
+    {T2,V2} = timer:tc(arith_prb,phi2,[X]),
+    {{T1,V1},{T2,V2},T1-T2}.
+
+test_avg(N) ->
+    test_avg(N,{0,0},N).
+test_avg(I,{AT1,AT2},N) when I > 0 ->
+    {{T1,_},{T2,_},_} = test_phi(10090),
+    test_avg(I-1,{AT1+T1/N,AT2+T2/N},N);
+test_avg(0,Acc,_) -> Acc.
