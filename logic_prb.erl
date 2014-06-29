dfabd09052c9950e86b01194818924e0656b353c
jepio <jeremi.piotrowski@gmail.com>
2014-06-30 00:00:10 +0200

    Added: solutions for problems up to 3.04.
    Next thing up: 3.05 which is the Huffman code, seems easy but I decided
    to understand binary trees first.

--- logic_prb.erl
+++ logic_prb.erl
@@ -1,9 +1,11 @@
 -module(logic_prb).
-
--compile(export_all).
+-export([land/2,lor/2,lnot/1,lnand/2,lnor/2,lxor/2,lequ/2,table/1,logic_table/1,
+         limpl/2,grey_code/1]).
+%%-compile(export_all).
 
 
 %% 3.01 Truth tables for logical expressions
+
 land(X,Y) ->
     case [X,Y] of
         [true,true] -> true;
@@ -24,8 +26,8 @@ lnand(X,Y) -> lnot(land(X,Y)).
 
 lnor(X,Y) -> lnot(lor(X,Y)).
 
-lxor(X,X) -> false;
-lxor(X,Y) -> true.
+lxor(_X,_X) -> false;
+lxor(_X,_Y) -> true.
 
 limpl(X,Y) ->
     case[X,Y] of
@@ -33,17 +35,14 @@ limpl(X,Y) ->
         _ -> true
     end.
 
-lequ(X,X) -> true;
-lequ(X,Y) -> false.
+lequ(_X,_X) -> true;
+lequ(_X,_Y) -> false.
 
 %% Function to print the logic table of a function
 %% Usage:
 %% logic_prb:table(fun logic_prb:limpl/2).
 table(F) when is_function(F,2) ->
-    Logic = [[true,true],
-             [true,false],
-             [false,true],
-             [false,false]],
+    Logic = logic_table(2), % was only added later 
     table(Logic,F).
 
 table([H|T],F) ->
@@ -59,4 +58,26 @@ table([],_) ->
 %% Some keywords that can be looked up here are parse transform
 %% or match specification.
 
+%% 3.03 Generalize the truth table for logical expressions taking arbitrary
+%% amounts of parameters.
+%% Unfortunately this also can't be done in Erlang, since function clauses and
+%% defined pattern matching are an integral part of the language.
+%% I can write a function that generates a table of logic cases.
 
+logic_table(1) ->
+    [[true], [false]];
+logic_table(N) ->
+    AllTrue=[ [true|X] || X <- logic_table(N-1)],
+    AllFalse=[ [false|X] || X <- logic_table(N-1)],
+    AllTrue ++ AllFalse. % I was considering to use | but it won't work,
+                         % I'd need to do it element by element.
+
+%% 3.04 Construct the N-bit grey code which is actually almost the same as 
+%% the above.
+
+grey_code(1) ->
+    ["0","1"];
+grey_code(N) ->
+    FirstPart = [string:concat("0",X) || X <- grey_code(N-1)],
+    SecondPart = [string:concat("1",X) || X <- lists:reverse(grey_code(N-1))],
+    FirstPart ++ SecondPart.
